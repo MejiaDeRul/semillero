f1a6ba59990cc1925ad3b4abeee45102f0908568
Mejia765 <101266677+Mejia765@users.noreply.github.com>
2024-09-20 13:50:43 -0500

    Creacion de base de la aplicacion

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Bebida(BaseModel):
+    name: str
+    price: int
+    cant: float
+
+class UpdateBebida(BaseModel):
+    id: int
+    new: Bebida
+
+app = FastAPI()
+
+bebidas = [
+    {'name': 'Coca Cola',
+     'price': 5000,
+     'cant': 1.5},
+    {'name': 'Sprite',
+     'price': 4000,
+     'cant': 2.5}
+]
