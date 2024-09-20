2f98b86baee8f11cf6e528551edbe789515af48b
Mejia765 <101266677+Mejia765@users.noreply.github.com>
2024-09-20 14:34:11 -0500

    Crud completo con lista

--- main.py
+++ main.py
@@ -3,28 +3,51 @@ from pydantic import BaseModel
 
 class Bebida(BaseModel):
     name: str
-    price: int
-    cant: float
+    precio: int
+    cantidad: float
 
 class UpdateBebida(BaseModel):
     id: int
-    new: Bebida
+    nuevo: Bebida
 
 app = FastAPI()
 
 bebidas = [
-    {'name': 'Coca Cola',
-     'price': 5000,
-     'cant': 1.5},
-    {'name': 'Sprite',
-     'price': 4000,
-     'cant': 2.5}
+    {
+        'name': 'Coca cola',
+        'precio': 5000,
+        'cantidad': 1.5
+    },
+    {
+        'name': 'Sprite',
+        'precio': 4000,
+        'cantidad': 2.5
+    }
 ]
 
 @app.get('/')
 def home():
-    return {'msg': 'hola'}
+    return {'msg': 'Hola'}
 
-@app.get('/bebida', tags=['Bebidas'])
+@app.get('/bebidas', tags=['Bebida'])
 def mostrar_bebidas():
     return {'bebidas': bebidas}
+
+@app.get('/bebidas/{id}', tags=['Bebida'])
+def mostrar_una_bebida(id: int):
+    return {'msg': bebidas[id]}
+
+@app.post('/bebidas', tags=['Bebida'])
+def crear_bebida(bebida: Bebida):
+    bebidas.append(bebida)
+    return {'msg': 'Nueva bebida creada'}
+
+@app.put('/bebidas', tags=['Bebida'])
+def actualizar_bebida(bebida: UpdateBebida):
+    bebidas[bebida.id] = bebida.nuevo
+    return {'msg': 'Bebida actualizada'}
+
+@app.delete('/bebidas', tags=['Bebida'])
+def eliminar_bebida(id: int):
+    bebidas.pop(id)
+    return {'msg': 'Bebida eliminada'}
